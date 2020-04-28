51d88585824a1f88e2336c9d6bf5639913342816
cra2ypierr0t <uchiyama.kazuhide@gmail.com>
2020-04-28 21:23:05 +0900

    この酷いコード達はいつか直す

--- 010/CompilationEngine.c
+++ 010/CompilationEngine.c
@@ -54,10 +54,18 @@ void class(){
     }
 
     while(1){
-        classVarDec();
+        if((keyWord() == KW_STATIC) || (keyWord() == KW_FIELD)){
+            classVarDec();
+        } else {
+            break;
+        }
     }
     while(1){
-        subroutineDec();
+    if((keyWord() == KW_CONSTRUCTOR) || (keyWord() == KW_FUNCTION) || (keyWord() == KW_METHOD)){
+            subroutineDec();
+        } else {
+            break;
+        }
     }
 
     if(symbol() == '}'){
@@ -69,68 +77,180 @@ void class(){
 }
     
 void classVarDec(){
-    if((keyWord() == KW_STATIC) || (keyWord() == KW_FIELD)){
-        printf("<classVarDec>\n\t");
-        printf("<keyword> %s </keyword>\n", stringVal());
-        advance();
-        printf("<keyword> %s </keyword>\n", stringVal());
+    printf("<classVarDec>\n\t");
+    printf("<keyword> %s </keyword>\n", stringVal());
+    advance();
+    printf("<keyword> %s </keyword>\n", stringVal());
+    advance();
+    printf("<identifier> %s </identifier>\n", identifier());
+    advance();
+    while(symbol() != ';'){
+        printf("<symbol> %c </symbol>\n", symbol());
         advance();
         printf("<identifier> %s </identifier>\n", identifier());
         advance();
-        while(symbol() != ';'){
-            printf("<symbol> %c </symbol>\n", symbol());
-            advance();
-            printf("<identifier> %s </identifier>\n", identifier());
-            advance();
-        }
-        printf("<symbol> %c </symbol>\n", symbol());
-        printf("</classVarDec>\n\t");
-        advance();
-    } else {
-        break; 
     }
+    printf("<symbol> %c </symbol>\n", symbol());
+    printf("</classVarDec>\n\t");
+    advance();
+    return;
 }
 
 void subroutineDec(){
-    if((keyWord() == KW_CONSTRUCTOR) || (keyWord() == KW_FUNCTION) || (keyWord() == KW_METHOD)){
-        printf("<subroutineDec>\n");
-        printf("<keyword> %s </keyword>\n", stringVal());
+    printf("<subroutineDec>\n");
+    printf("<keyword> %s </keyword>\n", stringVal());
+    advance();
+    printf("<keyword> %s </keyword>\n", stringVal());
+    advance();
+    printf("<identifier> %s </identifier>\n", identifier());
+    advance();
+    if(symbol() == '('){
+        printf("<symbol> %c </symbol>\n", symbol());
         advance();
-        printf("<keyword> %s </keyword>\n", stringVal());
-        advance();
-        printf("<identifier> %s </identifier>\n", identifier());
-        advance();
-        if(symbol() == '('){
-            printf("<symbol> %c </symbol>\n", symbol());
-            advance();
-            parameterList();
-        } else {
-            exit(1);
-        }
-        if(symbol() == ')'){
-            printf("<symbol> %c </symbol>\n", symbol());
-            advance();
-        } else {
-            exit(1);
-        }
-        subroutineBody();
-        printf("</subroutineDec>\n");
+        parameterList();
     } else {
-        break;
+        exit(1);
     }
+    if(symbol() == ')'){
+        printf("<symbol> %c </symbol>\n", symbol());
+        advance();
+    } else {
+        exit(1);
+    }
+    subroutineBody();
+    printf("</subroutineDec>\n");
+    return;
 }
 
 void parameterList(){
     if(symbol() == ')'){
         return;
     } else {
+        printf("<parameterList>\n");
         printf("<keyword> %s </keyword>\n", stringVal());
         advance();
         printf("<identifier> %s </identifier>\n", identifier());
         advance();
+        while(1){
+            if(symbol() == ','){
+                printf("<symbol> %c </symbol>\n", symbol());
+                advance();
+                printf("<keyword> %s </keyword>\n", stringVal());
+                advance();
+                printf("<identifier> %s </identifier>\n", identifier());
+                advance();
+            } else {
+                break;
+            }
+        }
+        printf("</parameterList>\n");
+        return;
     }
 }
 
 void subroutineBody(){
+    printf("<subroutineBody>\n");
+    if(symbol() == '{'){
+        printf("<symbol> %c </symbol>\n", symbol());
+        advance();
+    } else {
+        exit(1);
+    }
+    while(keyWord() == KW_VAR){
+        varDec();
+    }
+    statements();
+    if(symbol() == '}'){
+        printf("<symbol> %c </symbol>\n", symbol());
+        advance();
+    } else {
+        exit(1);
+    }
+    printf("</subroutineBody>\n");
+    return;
+}
+void varDec(){
+    printf("<varDec>\n");
+    printf("<keyword> %s </keyword>\n", stringVal());
+    advance();
+    printf("<keyword> %s </keyword>\n", stringVal());
+    advance();
+    printf("<identifier> %s </identifier>\n", identifier());
+    advance();
+    while(symbol() == ','){
+        printf("<symbol> %c </symbol>\n", symbol());
+        advance();
+        printf("<identifier> %s </identifier>\n", identifier());
+        advance();
+    }
+    if(symbol() == ';'){
+        printf("<symbol> %c </symbol>\n", symbol());
+        advance();
+    } else {
+        exit(1);
+    }
+    printf("</varDec>\n");
+    return;
+}
 
+void statements(){
+    printf("<statements>\n");
+    while(symbol() != '}'){
+        statement();
+    }
+    printf("</statements>\n");
+    return;
+}
+
+void statement(){
+    printf("<statement>\n");
+    if(keyWord() == KW_LET){
+        letStatement();
+    } else if(keyWord() == KW_IF){
+        ifStatement();
+    } else if(keyWord() == KW_WHILE){
+        whileStatement();
+    } else if(keyWord() == KW_DO){
+        doStatement();
+    } else if(keyWord() == KW_RETURN){
+        returnStatement();
+    } else {
+        exit(1);
+    }
+    printf("</statement>\n");
+    return;
+}
+
+void letStatement(){
+    printf("<letStatement>\n");
+    printf("<keyword> %s </keyword>\n", stringVal());
+    advance();
+    printf("<identifier> %s </identifier>\n", identifier());
+    advance();
+    if(symbol() == '['){
+        printf("<symbol> %c </symbol>\n", symbol());
+        advance();
+        expression();
+        if(symbol() == ']'){
+            printf("<symbol> %c </symbol>\n", symbol());
+            advance();
+        } else {
+            exit(1);
+        }
+    }
+    if(symbol() == '='){
+        printf("<symbol> %c </symbol>\n", symbol());
+        advance();
+    } else {
+        exit(1);
+    }
+    expression();
+    if(symbol() == ';'){
+        printf("<symbol> %c </symbol>\n", symbol());
+        advance();
+    } else {
+        exit(1);
+    }
+    printf("</letStatement>\n");
+    return;
 }
